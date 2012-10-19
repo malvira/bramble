81e9fc60ebecafbf0de8ee3ab15a3b171c1c8e60
root <root@alarm.red>
2012-10-19 17:02:39 -0500

    update mc1322x scripts and include build of mc1322x-load

--- bin/wanled.sh
+++ bin/wanled.sh
@@ -1,16 +1,16 @@
-#/bin/busybox
+#!/bin/busybox sh
 
 GPIO=453
 
 if [ ! -d /sys/class/gpio/gpio$GPIO ]
 then
-	busybox echo $GPIO > /sys/class/gpio/export
-	busybox echo "out" > /sys/class/gpio/gpio$GPIO/direction
+	echo $GPIO > /sys/class/gpio/export
+	echo "out" > /sys/class/gpio/gpio$GPIO/direction
 fi
 
 while /bin/true; do 
-	busybox echo 0 > /sys/class/gpio/gpio$GPIO/value
-	busybox sleep 1
-	busybox echo 1 > /sys/class/gpio/gpio$GPIO/value
-	busybox sleep 1
+	echo 0 > /sys/class/gpio/gpio$GPIO/value
+	sleep 1
+	echo 1 > /sys/class/gpio/gpio$GPIO/value
+	sleep 1
 done
