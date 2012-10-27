7212b2f2e6e22c2acd3279db9e3446dad9de59a1
Mariano Alvira <mar@devl.org>
2012-10-27 19:09:43 -0400

    add a script to setup bradmin on a debian machine.

---
+++ files/debian/install-debian.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+DIR=`pwd`
+BASE=$DIR/../..
+
+apt-get update 
+apt-get install -y git libssl0.9.8 nginx uwsgi uwsgi-plugins-all cython libjs-jquery python-flask python-pip python-dev ipv6calc apt-src
+pip install Flask-OpenID Flask-Login Flask-Principal Flask-Bcrypt nameparser Flask-Mako uwsgitop
+
+# get the uwsgi source, build the gevent plugin and install
+cd /tmp
+apt-src install uwsgi
+cd uwsgi-1.2.5+dfsg
+python uwsgiconfig.py --plugin plugins/gevent
+cp gevent_plugin.so /usr/lib/uwsgi/plugins
+
+# get gevent from source, build and install it
+cd /tmp
+git clone https://github.com/SiteSupport/gevent.git
+cd gevent
+python setup.py build
+python setup.py install
+
+# setup nginx
+STATIC=$BASE/web/bradmin
+cp etc/nginx/bradmin /etc/nginx/sites-available
+ln -s /etc/nginx/sites-available/bradmin /etc/nginx/sites-enabled/bradmin
+sudo sed -i "s,root PATH-TO-BRADMIN-STATIC,root $STATIC,g" /etc/nginx/sites-available/bradmin
+service nginx restart
+
+#setup uwsgi app
+cp ../arch/etc/uwsgi/apps/bradmin.ini /etc/uwsgi/apps-available
+ln -s /etc/uwsgi/apps-available/bradmin.ini /etc/uwsgi/apps-enabled/bradmin.ini
+service uwsgi restart
