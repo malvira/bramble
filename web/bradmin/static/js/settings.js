f6e8eb69577beefce4a32bc8ae9b5ab5863122e6
Mariano Alvira <mar@devl.org>
2012-11-05 21:01:46 -0500

    factor and cleanup default password setting code

--- web/bradmin/static/js/settings.js
+++ web/bradmin/static/js/settings.js
@@ -2,14 +2,17 @@ console.log('settings.js');
 
 var App = Em.Application.create({
     ready: function () { console.log('ready'); App.init()},
-    passChangeWait: false,
-    passChangeState: 'none',
+});
+
+App.password = Ember.Object.create({
+    ChangeWait: false,
+    ChangeState: 'none',
     pass1: "",
     pass2: "",
     changepass: function() { 
 	if(this.get('pass1') == this.get('pass2'))
 	{
-	    this.set('passChangeWait', true);
+	    this.set('ChangeWait', true);
 	    $.ajax({  
 		url: "settings/newpass",  
 		type: "POST",  
@@ -17,10 +20,9 @@ var App = Em.Application.create({
 		contentType: "application/json",  
 		data: JSON.stringify({ "password": this.get('pass1') }),  
 		success: function(data) {
-		    console.log('pass change ok');
-		    App.set('passChangeWait', false);
-		    App.set('pass1','');
-		    App.set('pass2','');
+		    App.password.set('ChangeWait', false);
+		    App.password.set('pass1','');
+		    App.password.set('pass2','');
 		}
 	    });  
 	}	
@@ -33,7 +35,7 @@ var App = Em.Application.create({
     match: function() {
 	return this.get('pass1') == this.get('pass2');
     }.property('pass1', 'pass2'),
-    passbad: function() {
+    bad: function() {
 	return !(this.get('match') && !this.get('empty'));
     }.property('match', 'empty')
 });
@@ -48,38 +50,6 @@ App.FadeInView = Ember.View.extend({
     }
 });
 
-
-/* try using find, addObject, and removeObject instead to manage the pings */
-
-/* initialization */
 App.init = function() {
-    App.tick.settimer();
 }
-
-App.ChangePass = function(pass1, pass2) {
-    console.log('called change pass');
-    console.log(pass1);
-    console.log(pass2);
-
-
-
-}
-
-/* debug views */
-App.tick = Ember.Object.create({
-    settimer: function () { setInterval(function() { App.tick.set('tick',App.tick.tick+1); }, 1000); },
-    tick: 0,
-    foo: "",
-    bar: function() {
-	console.log('bar');
-	return this.get('foo');
-    }.property('foo')
-});
-
-App.DebugView = Em.View.extend({
-    tickBinding: 'App.tick.tick',
-    fooBinding: 'App.tick.foo',
-    barBinding: 'App.tick.bar'
-});
-
 
