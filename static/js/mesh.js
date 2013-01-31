809e323b3c945d3d0f06b1fda9d2e7285df49aa4
Mariano Alvira <mar@devl.org>
2013-01-31 18:47:07 -0500

    inital web page with random mesh... needs tweaking.

---
+++ static/js/mesh.js
@@ -0,0 +1,188 @@
+var nodes;
+var links;
+
+var nodeToIdx = {};
+var linkToIdx = {};
+
+window.nodeFromEUI = function(eui) {
+    return nodes.findProperty('eui', eui);
+}
+
+window.addNode = function(node) {
+    var n = nodes.findProperty('eui', node.eui);
+    node.key = node.eui;
+    if (n) {
+	n = node;
+    } else {
+	nodes.push(node);
+    }
+    updateMesh();
+};
+
+window.removeNode = function(eui) {
+    var n = nodes.findProperty('eui', eui);
+    var rmLinks = [];
+
+    // find all the edges with this node and remove them
+    links.forEach(function(l) {
+	if (l.target.eui == eui || l.source.eui == eui) {
+	    rmLinks.pushObject(l);
+	}
+    });
+    rmLinks.forEach(function(l) { links.removeObject(l); });
+
+    if (n) {
+	nodes.removeObject(n);
+    }
+
+    updateMesh();
+}
+
+window.removeEdge = function(edge) {
+    var key = edge.source.eui + "->" + edge.target.eui;
+    var e = links.findProperty('key', key);
+    if (e) {
+	links.removeObject(e);
+    }
+    updateMesh();
+}
+
+window.addEdge = function(edge) {
+    var key = edge.source.eui + "->" + edge.target.eui;
+    edge.key = key;
+    var e = links.findProperty('key', key);
+    if (e) {
+	e.etx = edge.etx;
+	e.pref = edge.pref;
+    } else {
+	links.pushObject(edge);
+    }
+    updateMesh();
+};
+
+function resize() {
+    window.force.size([$("#mesh").width(), $(window).height()]);
+    var navh = $("#nav").height();
+    $("#mesh").height($(window).height() - navh - 75);
+    $("#nodes").height($(window).height() - navh - 75 );
+    $("#list").height(($(window).height() - navh) * .5);
+};
+
+$().ready(function() {
+    resize();
+});
+
+$(window).resize(function() {
+    resize();
+});
+
+function nodeClick(eui) {
+    node = nodeFromEUI(eui);
+    node.select();
+}
+
+function updateMesh() {
+
+    var link = svg.select("#links").selectAll(".link")
+	.data(links, function(l) { 
+	    return l.source.eui + "->" + l.target.eui;
+	} );
+
+    link.enter()
+	.append("line")
+	.attr("class", "link normal")
+        .style("opacity", 1e-6)
+	.transition()
+	 .duration(500)
+	.style("opacity", 1);
+
+    link.attr("class", function(l) {
+     	if (l.pref) {
+     	    return "link preferred";
+     	} else {
+     	    return "link normal";
+     	}});
+    link.attr("marker-end", function(l) {
+	if (l.pref) {
+	    return "url(#prefarrow)";
+	} else {
+	    return "url(#normarrow)";
+	}});
+   
+    link.exit()
+	.transition()
+	.duration(300)
+	.style("opacity", 1e-6)
+	.remove();
+
+    var node = svg.select("#nodes").selectAll(".node")
+	.data(nodes, function(n) { return n.eui; });
+
+    node.enter()
+	.append("circle")
+     	.attr("onclick", function(d) { return "nodeClick('" + d.eui + "')"})
+     	.attr("class", "node")
+     	.attr("id", function(d) { return "eui" + d.eui; })
+     	.attr("r", 10)
+        .style("opacity", 1e-6)
+	.transition()
+	 .duration(500)
+	.style("opacity", 1);
+
+    node.call(force.drag);
+
+    node.exit()
+	.transition()
+	.duration(500)
+	.style("opacity", 1e-6)
+	.remove();
+
+    force.start();
+
+}
+
+var svg;
+var force;
+
+(function() {
+
+    $("#mesh").height($(window).height());
+
+    svg = d3.select("#mesh").append("svg");
+    svg.append("defs").append("marker")
+	.attr("id", "prefarrow").attr("orient", "auto").attr("viewBox","0 0 20 20")
+	.attr("refX", "35").attr("refY", "10")
+	.append("path").attr("d", "M 0 0 L 20 10 L 0 20 z");
+    svg.select("defs").append("marker")
+	.attr("id", "normarrow").attr("orient", "auto").attr("viewBox","0 0 20 20")
+	.attr("refX", "55").attr("refY", "10").attr("stroke", "gray").attr("fill", "gray")
+	.append("path").attr("d", "M 0 0 L 20 10 L 0 20 z");
+    svg.append("g").attr("id","links");
+    svg.append("g").attr("id","nodes");
+
+    window.mesh = svg;
+
+    force = d3.layout.force()
+	.linkDistance(function(d) { if (d.etx > 1 ) { return d.etx * 100; } else { return 75; }})
+	.charge(-100)
+	.gravity(0.01)
+	.size([$("#mesh").width(), $(window).height()]);
+
+    nodes = force.nodes();
+    links  = force.links();
+
+    force.on("tick", function() {
+	svg.selectAll(".node")
+	    .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+	svg.selectAll(".link")
+	    .attr("x1", function(d) { return d.source.x; })
+	    .attr("y1", function(d) { return d.source.y; })
+	    .attr("x2", function(d) { return d.target.x; })
+	    .attr("y2", function(d) { return d.target.y; });
+    });
+
+    updateMesh();
+ 
+})(); 
+
---
+++ static/js/meshapp.js
@@ -0,0 +1,369 @@
+var App = Em.Application.create({
+    ready: function () { console.log('ready'); App.init()},
+    channel: null,
+    pollCount: 0,
+    brip: null,
+    getIP: function() {
+	$.ajax({  
+	    url: "radio/ip",  
+	    type: "GET",  
+	    dataType: "json",  
+	    contentType: "application/json",  
+	    success: function(data) {
+		if(data.status == 'error') { 
+		    window.setTimeout(App.getIP, 1000); 
+		    return; 
+		}
+		try {
+		    App.set('brip', data.addrs[0]);
+		    breui = ipv6ToIID(App.get('brip'));
+		    n = App.node.create({ eui: breui, addr: App.get('brip') });
+		    App.nodes.addIfNew(n);
+		    window.addNode(n);
+		    App.refreshNodes();
+		} catch(e) {
+		    window.setTimeout(App.getIP, 1000); 
+		}
+	    },
+	    error: function() {
+		window.setTimeout(App.getIP, 1000);
+	    }
+	});  
+    },
+    refreshNodes: function() {	
+	if(App.get('brip') == null) { return; }
+	$.ajax({  
+	    url: "coap",  
+	    type: "POST",  
+	    dataType: "json",  
+	    contentType: "application/json",  
+	    data: JSON.stringify({ 
+		"ip": App.get('brip'),
+		"method": "GET",
+		"path": "/rplinfo/routes"
+	    }),  
+	    success: function(data) {
+		resp = JSON.parse(data.response);
+		routes = resp.routes;
+		routes.forEach(function(r) {
+
+		    dest = ipv6ToIID(r.dest);
+
+		    n = App.node.create({ eui: dest, addr: r.dest });
+		    App.nodes.addIfNew(n);
+		    window.addNode(n);
+		    n.getParents();		
+		})
+	    }
+	});  	
+    }
+
+});
+
+/* get IID from an ipv6 device */
+ipv6ToIID = function(ip) {
+    // this is a bit of a hack
+    // we want to deal with euis but we get routes back from the border router
+    // assume that the IP addresses are derived from the IID and assume the last 64bits
+    // also assume the :: isn't in the IID
+    elts = ip.split(':');
+    // pad each with leading zeros
+    ip = elts.slice(elts.length-4,elts.length)
+    
+    ip.forEach(function(i) {
+	var h = parseInt(i, 16);
+	this[this.indexOf(i)] = ("0000" + h.toString(16)).substr(-4);
+    }, ip);
+    
+    ip = ip.join('');
+    return ip;
+}
+
+App.randomMeshAction = function() {
+    setTimeout(App.randomMeshAction, 500 + Math.floor(Math.random()*1500));
+    var ran = Math.floor(Math.random()*3)
+    if (ran == 1) {
+	// node
+	ran = Math.floor(Math.random()*5)
+	if (ran > 1) {
+	    // add node
+	    console.log("add node");
+	    var n = {};
+	    n.eui = String(Math.floor(Math.random()*32))	    
+	    console.log(n);
+	    addNode(n);
+	} else {
+	    // remove node
+	    console.log("remove node");
+	    var n = nodes[Math.floor(Math.random()*links.length)]
+	    console.log(n);
+	    if (n) { removeNode(n.eui); }
+	}
+    } else {
+	// edge
+	ran = Math.floor(Math.random()*5)
+	if (ran > 1) {
+	    // add edge
+	    console.log("add edge");
+	    var e = {};
+	    e.etx = Math.floor(Math.random()*4);
+	    var s = Math.floor(Math.random()*nodes.length);
+	    e.source = nodes[s];
+	    var t;
+	    while (t == s) {
+		t = Math.floor(Math.random()*nodes.length);
+	    }
+	    console.log(s)
+	    console.log(t)	    
+	    e.target = nodes[t];
+	    ran = Math.floor(Math.random()*3)
+	    if (ran == 1) {
+		e.pref = false;
+	    } else {
+		e.pref = true;
+	    }
+	    console.log(e);
+	    addEdge(e);
+	} else {
+	    // remove edge
+	    console.log("remove edge");
+	    var e = links[Math.floor(Math.random()*links.length)]
+	    console.log(e);
+	    if (e) { removeEdge(e); }
+	}
+    }
+
+}
+
+App.init = function() {
+    App.nodeListView.appendTo('#list');
+    App.nodeDetailsView.appendTo('#details');
+
+    App.getIP();
+    setInterval(App.refreshNodes, 10000);
+    App.poll();
+    setTimeout(App.randomMeshAction, 1000);
+    addNode({"eui":"a"});
+    addNode({"eui":"b"});
+    addNode({"eui":"c"});
+    addNode({"eui":"d"});
+
+    addEdge({"etx": 2, "source": nodes[0], "target": nodes[1]});
+    addEdge({"etx": 2, "source": nodes[0], "target": nodes[2]});
+    addEdge({"etx": 2, "source": nodes[0], "target": nodes[3]});
+
+
+};
+
+App.nodes = Em.ArrayController.create({
+    content: [],
+    addIfNew: function(n) {
+	if (this.findProperty('eui', n.eui) == null) {
+	    this.pushObject(n);
+	    var c = this.get('content');
+	    c.sort(function(a,b) {
+		return a.eui - b.eui;
+	    });
+	    this.replace(0,c.length,c);
+	}
+    },
+});
+
+App.nodeListView = Ember.View.create({
+    templateName: 'node-list',
+});
+
+App.nodeDetailsView = Ember.View.create({
+    templateName: 'node-details',
+});
+
+App.nodeItemView = Ember.View.extend({
+    classNameBindings: ['content.selected', 'content.focus'],
+    template: Ember.Handlebars.compile('{{view.content.eui}}'),
+    mouseEnter: function(event, view) {
+    	this.content.set('focus', true);
+    	this.$().animate({
+    	    backgroundColor: "#abcdef"
+    	}, 250 );
+    },
+    mouseLeave:  function(event, view) {
+    	this.content.set('focus', false);
+    	this.$().animate({
+    	    backgroundColor: "#ffffff"
+    	}, 75 );	
+    },
+    click: function(e,v) {
+    	this.content.select();
+    }
+});
+
+App.selectedNode = null;
+App.node = Em.Object.extend({
+    eui: null,
+    addr: null,
+    coapURL: function() {
+	return "coap://[" + this.get('addr') + "]";
+    }.property('addr'),
+    selected: false,
+    focus: false,
+    select: function() {
+	if(App.selectedNode) { 
+	    App.selectedNode.set('selected', false); 
+	    window.mesh.selectAll("#eui" + App.selectedNode.eui).style("stroke", "black");
+	}
+	this.set('selected', true);
+	App.set('selectedNode', this);
+	var eui = App.get('selectedNode').get('eui');
+	window.mesh.selectAll("#eui" + eui).style("stroke", "red");
+    },
+    clearEdges: function() {
+	console.log(this.eui + " clear edges");
+    },
+    getParents: function() {
+	var eui = this.eui;
+	var n = this
+	$.ajax({  
+	    url: "coap",  
+	    type: "POST",  
+	    dataType: "json",  
+	    contentType: "application/json",  
+	    data: JSON.stringify({ 
+		"ip": this.get('addr'),
+		"method": "GET",
+		"path": "/rplinfo/parents"
+	    }),
+	    success: function(data) {
+		
+		try
+		{
+		    var resp = JSON.parse(data.response);
+		    resp.parents.forEach(function(p) {
+			window.addNode(p);
+			n = App.node.create({ eui: p.eui })
+			App.nodes.addIfNew(n)
+			
+			var edge = {};
+			edge.etx = p.etx/128;
+			edge.source = window.nodeFromEUI(eui);
+			edge.target = window.nodeFromEUI(p.eui);
+			edge.pref = p.pref;
+			window.addEdge(edge);
+		    });
+		}
+		catch(e)
+		{
+		    if (data.response == '') {
+			// should remove all this nodes edges here
+			n.clearEdges();
+		    } else {
+			console.log('invalid json');
+			console.log(data.response);
+		    }
+		}
+
+	    }
+	});  
+    }
+});
+
+App.getChan = function() {
+    $.ajax({  
+	url: "channel",  
+	type: "POST",  
+	dataType: "json",  
+	contentType: "application/json",  
+	data: JSON.stringify({ "events": ["brsChanged", "ping"] }),  
+	success: function(data) {
+	    App.set('channel', data.channel);
+	    App.poll();
+	}
+    });  
+}
+
+App.poll = function () {
+    if (App.get('pollCount') == 0) {
+	if(App.get('channel') == null) {
+	    App.getChan();	    
+	} else {
+	    App.set('pollCount', App.pollCount + 1);
+	    $.ajax({  
+		url: "channel/" + App.get('channel'),  
+		type: "GET",  
+		dataType: "json",  
+		success: function(data) {
+		    App.set('pollCount', App.pollCount - 1);
+		    App.doChanData(data);
+		    App.poll();
+		},
+		error: function(xhr, status, err) {
+		    App.set('pollCount', App.pollCount - 1);
+		    if(status == 'error') {
+			setTimeout( function() { App.set('channel', null); App.poll(); }, 15000);
+		    } else {
+			App.poll();
+		    }
+		}
+	    });  
+	}
+    } else {
+	setTimeout( function() { App.poll(); }, 15000);
+    }
+};    
+
+App.doChanData = function(data) {
+    if (data != null) {
+	if ('event' in data) {
+	    if (data.event.name == 'rplData') {
+		if ('routes' in data.event) {
+		    data.event.routes.forEach( function(item) {
+			console.log(item);
+		    });
+		}
+		if ('rank' in data.event) {
+		    var node = {};
+		    node.eui = data.event.src;
+		    node.rank = data.event.rank;
+		    n = App.node.create({ eui: node.eui, rank: node.rank });
+		    App.nodes.addIfNew(n);
+		    window.addNode(node);
+		}
+		if ('adr' in data.event) {
+		    var src = {};
+		    var target = {};
+		    src.eui = data.event.src;
+		    target.eui = data.event.adr;
+		    window.addNode(src);
+		    window.addNode(target);
+		    
+		    s = App.node.create({ eui: src.eui })
+		    t = App.node.create({ eui: target.eui })
+		    App.nodes.addIfNew(s)
+		    App.nodes.addIfNew(t)
+
+		    var d = new Date();
+		    var edge = {};
+		    edge.time = d.getTime();
+		    edge.etx = data.event.etx/128;
+		    edge.source = data.event.src;
+		    edge.target = data.event.adr;
+		    edge.pref = data.event.pref;
+		    window.addEdge(edge);
+		}
+	    };
+	};
+    };
+};
+
+App.viewInfoMesh = Ember.View.extend({
+    nodeBinding: 'App.nodes'
+});
+
+App.nodeItem = Ember.View.extend({
+    templateName: 'node-item',
+    euiBinding: null,
+    selectedBinding: false,
+});
+
+App.selectedNodeView = Ember.View.extend({
+    nodeBinding: 'App.selectedNode',
+});
